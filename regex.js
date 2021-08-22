9e970dd333f55a44520f6e519fb913ff21973c03
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-08-22 13:08:27 -0700

    Update regex.js

--- regex.js
+++ regex.js
@@ -52,6 +52,7 @@
 //  /(abc)/	A group
 //  /a|b|c/	Any one of several patterns
 
+                    
 //  /\d/	The shortcut to look for digit characters is \d, with a lowercase d. This is equal to the character class [0 - 9], which looks for a single 
 //        character of any number between zero and nine.
 
