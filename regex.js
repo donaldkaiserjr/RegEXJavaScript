09a23338504a4e3c512df88c482cc1eec0574bb8
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-08-22 13:09:47 -0700

    Update regex.js

--- regex.js
+++ regex.js
@@ -289,6 +289,7 @@ let testStr = "Pumpkin";
 let testRegex = /P(engu|umpk)in/;
 testRegex.test(testStr);   // Returns true
 
+
 // Example 2 of Check for Mixed Grouping of Characters
 // Fix the regex so that it checks for the names of Franklin Roosevelt or Eleanor Roosevelt in a case sensitive manner and it should make concessions for middle names.
 // Then fix the code so that the regex that you have created is checked against myString and either true or false is returned depending on whether the regex matches.
