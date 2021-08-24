4f4e5a00438b9f0e7946bf78354bfa5a8b565235
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-08-24 11:34:03 -0700

    Update regex.js

--- regex.js
+++ regex.js
@@ -164,6 +164,7 @@ let bagStr = "bag";
 let bugStr = "bug";
 let bogStr = "bog";
 let bgRegex = /b[aiu]g/;
+
 bigStr.match(bgRegex); // Returns ["big"]
 bagStr.match(bgRegex); // Returns ["bag"]
 bugStr.match(bgRegex); // Returns ["bug"]
