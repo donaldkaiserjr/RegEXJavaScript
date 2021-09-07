7643a90f6d8d36fdb4368cbfa3a0faeb48018ef3
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-09-07 15:51:27 -0700

    Update regex.js

--- regex.js
+++ regex.js
@@ -100,6 +100,7 @@
 
      
                     
+                    
 // \cX	Matches a control character using caret notation, where "X" is a letter from A–Z (corresponding to codepoints U+0001–U+001A). For example, /\cM\cJ/ matches "\r\n".
 
                     
