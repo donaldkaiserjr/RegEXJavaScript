4d3b74d4b3c02410f023a13b4f69f779766c3ff2
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-09-07 15:53:12 -0700

    Update regex.js

--- regex.js
+++ regex.js
@@ -4,7 +4,6 @@
 // g flag:  To search or extract a pattern more than once, you can use the 'g' flag.
 
 
-
 // ex:   let repeatRegex = /Repeat/g;
           let testStr = "Repeat, Repeat, Repeat";
           let ourRegex = /Repeat/g;
