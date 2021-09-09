74b4dd10f7df615bcf5de865061ff8321f85c9f7
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-09-09 14:07:15 -0700

    Update regex.js

--- regex.js
+++ regex.js
@@ -341,7 +341,6 @@ wrongText.replace(silverRegex, "blue");// Returns "The sky is blue."
 
 
 
-
 // Remove Whitespace from Start and End
 let hello = "   Hello, World!  ";
 let wsRegex = /^\s+|\s+$/g; // 
