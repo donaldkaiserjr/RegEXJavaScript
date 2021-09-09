690a140bbfcb99d17a030c00dffdd305eff0db11
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-09-09 13:52:47 -0700

    Update regex.js

--- regex.js
+++ regex.js
@@ -100,8 +100,7 @@
 // \0	Matches a NUL character. Do not follow this with another digit.
 
      
-                    
-                    
+                                  
 // \cX	Matches a control character using caret notation, where "X" is a letter from A–Z (corresponding to codepoints U+0001–U+001A). For example, /\cM\cJ/ matches "\r\n".
 
                     
