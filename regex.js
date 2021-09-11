56322803f94a9413b8582298dfcb4679f44c2bfb
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-09-11 15:47:55 -0700

    Update regex.js

--- regex.js
+++ regex.js
@@ -384,6 +384,7 @@ You need to check all the usernames in a database. Here are some simple rules th
 
 
 
+
 */
 let username = "JackOfAllTrades";
 let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i; 
