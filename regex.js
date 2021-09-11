21c9b305c619c7e7858ece8791f7130dcf0c9f1e
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-09-11 15:50:23 -0700

    Update regex.js

--- regex.js
+++ regex.js
@@ -15,7 +15,6 @@
 
 
 
-
 // ^ symbol: ^ (carrot) creates a set of characters that you DON'T want to match. Place a caret character (^) after the opening bracket and before the 
 //          characters you do not want to match. For example, /[^aeiou]/gi matches all characters that are not a vowel
 //          It can also search for patterns at the beginning of strings.
